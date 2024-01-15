d2c3ba966f8c5cada52e9bca8b62d728b4f5096b
sumtuckyi <sumin510@naver.com>
2024-01-16 00:47:22 +0900

    router 추가

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,27 +1,40 @@
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-
+import './App.scss';
 
 const Home = () => <div>Home Page</div>;
 const About = () => <div>About Page</div>;
+const Menu1 = () => <div>Menu1 Page</div>;
 
+function NavBar() {
+  return (
+    <div className="nav">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/menu1">Menu1</Link>
+          </li>
+
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/menu1" element={<Menu1 />} />
+      </Routes>
+    </div>
+  )
+}
 function App() {
   return (
     <div className="App">
       <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/about" component={About} />
-          <Route path="/" component={Home} />
-        </Routes>
+        <NavBar />
       </Router>
     </div>
   );
